69d5150ece1c83525929929bb2fa829c533e9961
Nathan Rooy <nathanrooy@gmail.com>
2022-01-16 17:54:24 -0500

    Create longtinyloop.c

---
+++ src/longtinyloop.c
@@ -0,0 +1,10 @@
+double longtinyloopscore(pts) {
+
+    /* compute total path length */
+    
+    /* compute convex hull */
+   
+    /* compute rotating calipers distance */
+    
+    /* return: chull, rc_p1, rc_p2, path_len */
+}
